4e30614e4385212f16c7159554e624f47aaa86cd
rickxy <36708000+rickxy@users.noreply.github.com>
2022-03-16 11:25:55 +0300

    updates

--- CustomerHome/urls.py
+++ CustomerHome/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
+
 
 urlpatterns = [
     path('', views.index, name="Home"),
